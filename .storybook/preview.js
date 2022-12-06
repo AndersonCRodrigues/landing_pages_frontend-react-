61bc20a3bf0637f0f87e6f65da88c8950f8050fd
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-06 09:11:56 -0300

    exemplo de utilização do storyBook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,20 @@ import { theme } from '../src/styles/theme'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ]
+  }
+};
 
 export const decorators = [
   (Story) => (
