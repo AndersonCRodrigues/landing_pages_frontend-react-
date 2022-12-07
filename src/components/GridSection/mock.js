ed190d8de6f9b152aa428b18d081c71bc84be79b
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-07 11:41:45 -0300

    add component gridtext

---
+++ src/components/GridSection/mock.js
@@ -0,0 +1,23 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."',
+  grid: [
+    {
+      title: 'teste 1',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+      // srcImg: 'http://source.unsplash.com/random/360x360?r=1',
+    },
+    {
+      title: 'teste 2',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+      // srcImg: 'http://source.unsplash.com/random/360x360?r=1',
+    },
+    {
+      title: 'teste 3',
+      description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`,
+      // srcImg: 'http://source.unsplash.com/random/360x360?r=1',
+    },
+  ],
+};
