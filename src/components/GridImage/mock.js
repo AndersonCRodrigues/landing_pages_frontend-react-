918813fc7c0e01f190301e3b8ac34ea91a6d6329
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-07 12:21:04 -0300

    add component gridimage

---
+++ src/components/GridImage/mock.js
@@ -0,0 +1,32 @@
+export default {
+  background: false,
+  title: 'My grid',
+  description:
+    '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."',
+  grid: [
+    {
+      altText: 'teste 1',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=1',
+    },
+    {
+      altText: 'teste 2',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=2',
+    },
+    {
+      altText: 'teste 3',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=3',
+    },
+    {
+      altText: 'teste 4',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=4',
+    },
+    {
+      altText: 'teste 5',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=5',
+    },
+    {
+      altText: 'teste 6',
+      srcImg: 'http://source.unsplash.com/random/800x800?r=6',
+    },
+  ],
+};
