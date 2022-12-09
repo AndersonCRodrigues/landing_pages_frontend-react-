01ade1c9044f899b970707278344f52b0f066335
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-09 16:21:58 -0300

    add router dom

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,16 @@ import { GlobalStyles } from './styles/global-styles';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { Home } from './templates/Home/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <Home />
-    <GlobalStyles />
-  </ThemeProvider>,
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route to="*" component={Home} />
+      </Switch>
+      <GlobalStyles />
+    </ThemeProvider>
+  </BrowserRouter>,
 );
