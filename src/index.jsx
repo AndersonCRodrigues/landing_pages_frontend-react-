fe6524ba5db5c4f9017edde0811452bf87132926
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-09 16:36:19 -0300

    altera home.jsx para index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyles } from './styles/global-styles';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import { Home } from './templates/Home/Home';
+import { Home } from './templates/Home';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
